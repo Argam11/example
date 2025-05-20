0df65b27e0059efda9da4cef023a6513a507abe2
Argam11 <45657855+Argam11@users.noreply.github.com>
2025-05-20 11:32:16 +0000

    a

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
-console.log(1);
+window.addEventListener("deviceorientation", handleOrientation, true);
+//    window.addEventListener("deviceorientationabsolute", handleOrientation, true);
 
 
-throw 55;
+var printTimeout;
+var lastPrintEl;
+var errorShown = false;
 
+function handleOrientation(event) {
+  try {
+    var alpha = event.alpha;
+    var beta = event.beta;
+    var gamma = event.gamma;
 
-console.log(1.2);
+    // JS math works in radians, so convert
+    var betaR = beta / 180 * Math.PI;
+    var gammaR = gamma / 180 * Math.PI;
+    var spinR = Math.atan2(Math.cos(betaR) * Math.sin(gammaR), Math.sin(betaR));
+    // convert back to degrees
+    var spin = spinR * 180 / Math.PI;
+
+    // throttle output
+    if (printTimeout == undefined) {
+      var outEl = document.getElementById("rotationOutput");
+      var element = document.createElement("div");
+      element.innerHTML = ""
+        + "\tSpin = " + Math.floor(spin)
+        + "\tAlpha = " + Math.floor(alpha)
+        + "\tBeta = " + Math.floor(beta)
+        + "\tGamma = " + Math.floor(gamma);
+      if (lastPrintEl)
+        outEl.insertBefore(element, lastPrintEl);
+      else {
+        outEl.innerHTML = ""; // clear warning message
+        outEl.appendChild(element);
+      }
+      lastPrintEl = element;
+      printTimeout = setTimeout(function () {
+        printTimeout = undefined;
+      }, 3000);
+    }
+  }
+  catch (ex) {
+    if (!errorShown) {
+      errorShown = true;
+      alert(ex);
+    }
+  }
+}
--- script2.js
+++
@@ -1,6 +0,0 @@
-
-console.log(2);
-
-throw 233;
-
-console.log(2.2);
